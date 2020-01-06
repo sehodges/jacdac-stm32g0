c18687d9cca8fff0919a6f5b167d9df535704e30
Michal Moskal <michal@moskal.me>
2020-01-06 18:30:23 +0100

    Switch to TIM17

--- src/tim.c
+++ src/tim.c
@@ -3,8 +3,32 @@
 #include "stm32g0xx_ll_bus.h"
 #include "stm32g0xx_ll_utils.h"
 
-void set_timer(int delta) {
-    LL_TIM_OC_SetCompareCH1(TIM1, TIM1->CNT + delta);
+#define TIMx TIM17
+#define TIMx_IRQn TIM17_IRQn
+#define TIMx_IRQHandler TIM17_IRQHandler
+#define TIMx_CLK LL_APB2_GRP1_PERIPH_TIM17
+
+static volatile uint64_t timeoff;
+
+uint64_t get_micros() {
+    while (1) {
+        uint32_t v0 = TIMx->CNT;
+        uint64_t off = timeoff;
+        uint32_t v1 = TIMx->CNT;
+        if (v0 <= v1)
+            return off + v1;
+    }
+}
+
+static cb_t timer_cb;
+static void *timer_cb_arg;
+
+void set_timer(int delta, cb_t cb, void *arg) {
+    if (delta < 10)
+        delta = 10;
+    timer_cb = cb;
+    timer_cb_arg = arg;
+    LL_TIM_OC_SetCompareCH1(TIMx, TIMx->CNT + delta);
 }
 
 void tim_init() {
@@ -15,29 +39,25 @@ void tim_init() {
     LL_TIM_InitTypeDef TIM_InitStruct = {0};
 
     /* Peripheral clock enable */
-    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
+    LL_APB2_GRP1_EnableClock(TIMx_CLK);
 
-    /* TIM1 interrupt Init */
-    NVIC_SetPriority(TIM1_BRK_UP_TRG_COM_IRQn, 0);
-    NVIC_EnableIRQ(TIM1_BRK_UP_TRG_COM_IRQn);
-
-    NVIC_SetPriority(TIM1_CC_IRQn, 0);
-    NVIC_EnableIRQ(TIM1_CC_IRQn);
+    NVIC_SetPriority(TIMx_IRQn, 0);
+    NVIC_EnableIRQ(TIMx_IRQn);
 
     TIM_InitStruct.Prescaler = tim_prescaler;
     TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
     TIM_InitStruct.Autoreload = 0xffff;
     TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
     TIM_InitStruct.RepetitionCounter = 0;
-    LL_TIM_Init(TIM1, &TIM_InitStruct);
-    LL_TIM_DisableARRPreload(TIM1);
-    LL_TIM_SetClockSource(TIM1, LL_TIM_CLOCKSOURCE_INTERNAL);
-    LL_TIM_SetTriggerOutput(TIM1, LL_TIM_TRGO_RESET);
-    LL_TIM_SetTriggerOutput2(TIM1, LL_TIM_TRGO2_RESET);
-    LL_TIM_DisableMasterSlaveMode(TIM1);
+    LL_TIM_Init(TIMx, &TIM_InitStruct);
+    LL_TIM_DisableARRPreload(TIMx);
+    LL_TIM_SetClockSource(TIMx, LL_TIM_CLOCKSOURCE_INTERNAL);
+    LL_TIM_SetTriggerOutput(TIMx, LL_TIM_TRGO_RESET);
+    LL_TIM_SetTriggerOutput2(TIMx, LL_TIM_TRGO2_RESET);
+    LL_TIM_DisableMasterSlaveMode(TIMx);
 
-    LL_TIM_ClearFlag_UPDATE(TIM1);
-    LL_TIM_EnableIT_UPDATE(TIM1);
+    LL_TIM_ClearFlag_UPDATE(TIMx);
+    LL_TIM_EnableIT_UPDATE(TIMx);
 
     LL_TIM_OC_InitTypeDef TIM_OC_InitStruct = {0};
     TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_FROZEN;
@@ -48,35 +68,39 @@ void tim_init() {
     LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH1, &TIM_OC_InitStruct);
     LL_TIM_OC_DisableFast(TIM3, LL_TIM_CHANNEL_CH1);
 
-    LL_TIM_EnableIT_CC1(TIM1);
+    LL_TIM_EnableIT_CC1(TIMx);
 
-    LL_TIM_EnableCounter(TIM1);
+    LL_TIM_EnableCounter(TIMx);
 
-    set_timer(5000);
+    set_timer(5000, NULL, NULL);
 
     /* Configure the Cortex-M SysTick source to have 1ms time base */
     // LL_Init1msTick(SystemCoreClock);
     //    LL_mDelay(200);
 }
 
-/*
- * @brief This function handles TIM1 break, update, trigger and commutation interrupts.
- */
-void TIM1_BRK_UP_TRG_COM_IRQHandler(void) {
+void TIMx_IRQHandler() {
     /* Check whether update interrupt is pending */
-    if (LL_TIM_IsActiveFlag_UPDATE(TIM1) == 1) {
+    if (LL_TIM_IsActiveFlag_UPDATE(TIMx) == 1) {
         /* Clear the update interrupt flag */
-        LL_TIM_ClearFlag_UPDATE(TIM1);
+        LL_TIM_ClearFlag_UPDATE(TIMx);
+        timeoff += 0x10000;
 
         pulse_log_pin();
         pulse_log_pin();
     }
-}
 
-void TIM1_CC_IRQHandler() {
-    if (LL_TIM_IsActiveFlag_CC1(TIM1) == 1) {
-        LL_TIM_ClearFlag_CC1(TIM1);
+    if (LL_TIM_IsActiveFlag_CC1(TIMx) == 1) {
+        LL_TIM_ClearFlag_CC1(TIMx);
+
         pulse_log_pin();
-        set_timer(5000);
+
+        cb_t f = timer_cb;
+        void *arg = timer_cb_arg;
+
+        set_timer(10000, NULL, NULL);
+
+        if (f)
+            f(arg);
     }
 }
