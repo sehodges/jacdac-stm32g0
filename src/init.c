67565ee19f917f5c0264bc65f4f2abd6a8447d73
Michal Moskal <michal@moskal.me>
2020-01-20 13:07:48 -0800

    LL init

--- src/init.c
+++ src/init.c
@@ -1,13 +1,5 @@
 #include "jdsimple.h"
 
-void HAL_MspInit(void) {
-    __HAL_RCC_SYSCFG_CLK_ENABLE();
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-}
-
 void NMI_Handler(void) {}
 
 void HardFault_Handler(void) {
@@ -18,12 +10,15 @@ void SVC_Handler(void) {}
 
 void PendSV_Handler(void) {}
 
-void SysTick_Handler(void) {
-    HAL_IncTick();
-}
+void SysTick_Handler(void) {}
+
+uint32_t SystemCoreClock;
 
 static void enable_nrst_pin() {
 #ifdef STM32G0
+#define FLASH_KEY1 0x45670123U
+#define FLASH_KEY2 0xCDEF89ABU
+
     DMESG("check NRST", FLASH->OPTR & FLASH_OPTR_NRST_MODE);
 
     if (FLASH->OPTR & FLASH_OPTR_NRST_MODE_0)
@@ -31,7 +26,7 @@ static void enable_nrst_pin() {
 
     uint32_t nrstmode;
 
-    /* Enable Flash access anyway */
+        /* Enable Flash access anyway */
     __HAL_RCC_FLASH_CLK_ENABLE();
 
     /* Unlock flash */
@@ -67,39 +62,50 @@ static void enable_nrst_pin() {
 }
 
 void SystemClock_Config(void) {
-#ifdef STM32G0
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    uint32_t clock_mhz;
 
-    // Configure the main internal regulator output voltage
-    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
-    // Initializes the CPU, AHB and APB busses clocks
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
-    RCC_OscInitStruct.PLL.PLLN = 8;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        panic();
-    }
-    // Initializes the CPU, AHB and APB busses clocks
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        panic();
-    }
+#if defined(STM32G0)
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 8, LL_RCC_PLLR_DIV_2);
+    clock_mhz = 64;
+#elif defined(STM32F0)
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2, LL_RCC_PLL_MUL_12);
+    clock_mhz = 48;
 #else
-// #warning TODO
+#error "clock?"
 #endif
 
+    LL_RCC_PLL_Enable();
+#ifdef RCC_PLLCFGR_PLLREN
+    LL_RCC_PLL_EnableDomain_SYS();
+#endif
+    while (LL_RCC_PLL_IsReady() != 1)
+        ;
+
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+        ;
+
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+    SystemCoreClock = clock_mhz * 1000000;
+    LL_InitTick(SystemCoreClock, 1000U);
+    LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
     enable_nrst_pin();
+}
+
+void SystemInit(void) {
+#ifdef STM32G0
+    SCB->VTOR = FLASH_BASE;
+#endif
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    SystemClock_Config();
 }
