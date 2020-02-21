7718c2a10db94fc0cf61ef30da98add79767911a
Michal Moskal <michal@moskal.me>
2020-02-20 16:06:05 -0800

    Align with latest proto

--- src/jdprotocol.h
+++ src/jdprotocol.h
@@ -9,12 +9,18 @@ extern "C" {
 #endif
 
 #define JD_SERIAL_PAYLOAD_SIZE 255
+#define JD_SERIAL_VERSION 1
 
 typedef struct {
     uint16_t crc;
-    uint8_t size;
-    uint8_t service_number;
+    uint8_t version; // JD_SERIAL_VERSION
+    uint8_t serial_flags;
     uint64_t device_identifier;
+    uint8_t size; // of the payload
+    uint8_t service_number;
+    uint8_t service_command;
+    uint8_t service_arg;
+    uint8_t data[0];
 } __attribute((__packed__)) __attribute__((aligned(4))) jd_packet_header_t;
 
 typedef struct {
